74d57096e7b5b90a576158ca42b9ca51c8abecc4
shiva <you@example.com>
2019-03-29 12:58:07 +0530

    mycricket project contine here

---
+++ home_app/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from pycricbuzz import Cricbuzz
+import json
+import requests
+c = Cricbuzz()
+
+
+def home(request):
+    matches = c.matches()
+    send = {'matches' : matches}
+    return render(request, 'home_app/home.html', send)
+
+def new_home(request):
+    matches = c.matches()
+    send = {'matches' : matches}
+    list_mathes = []
+    for i in range(len(matches)):
+        list_mathes.append(matches[i]['id'])
+    live_match = []
+    complt_match = []
+    preview_match =[]
+    for i in list_mathes:
+        check = c.matchinfo(i)
+        if check['mchstate'] == 'inprogress':
+            live_match.append(check)
+        elif check['mchstate'] ==  'innings break':
+            live_match.append(check)
+        elif check['mchstate'] == 'preview':
+            preview_match.append(check)
+        else:
+            complt_match.append(check)
+    send['live'] = live_match
+    send['complited'] = complt_match
+    send['preview'] = preview_match
+    print(send['live'])
+    return render(request, 'home_app/new_home.html', send)
---
+++ scorepage/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render,HttpResponse
+from home_app.views import c
+
+
+def score(request, id):
+   score = c.scorecard(id)
+   info = c.matchinfo(id)
+   commentary = c.commentary(id)
+   print(commentary)
+   send = {      'matchinfo': info,
+                 'commentary':commentary['commentary'],
+                 }
+   try:
+               send['stat']     = info['mchstate']
+               send['team1']      = score['scorecard'][0]
+               send['bowlcard_1'] = score['scorecard'][0]['bowlcard']
+               send['team2']      = score['scorecard'][1]
+               send['bowlcard_2'] = score['scorecard'][1]['bowlcard']
+   except:
+        try:
+              send['stat']     = info['mchstate']
+              send['team1']      = score['scorecard'][0]
+              send['bowlcard_1'] = score['scorecard'][0]['bowlcard']
+
+        except:
+             send['stat']  = info['mchstate']
+   return render(request, 'score/score.html', send)
