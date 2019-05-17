e0c258574c7c5671f5ed4c5be06d39a19b3885d5
shiva <you@example.com>
2019-05-17 20:19:50 +0530

    upxdate

--- staticfiles/js/home.874259b9ffad.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("load", function() {
-    var f = document.getElementById('Foo');
-    setInterval(function() {
-        f.style.display = (f.style.display == 'none' ? '' : 'none');
-    }, 1000);
-
-}, false);
